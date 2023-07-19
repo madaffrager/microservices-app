fd5b846c96d59c4d77fdae1074cce5e1b2789802
madaffrager <84815708+madaffrager@users.noreply.github.com>
2023-07-19 18:35:05 +0100

    RabbitListener setup in notification Microservice

--- notification/src/main/java/com/github/madaffrager/notification/NotificationApplication.java
+++ notification/src/main/java/com/github/madaffrager/notification/NotificationApplication.java
@@ -3,7 +3,9 @@ package com.github.madaffrager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = "com.github.madaffrager.amqp"
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
